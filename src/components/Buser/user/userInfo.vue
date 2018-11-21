a901b46d6c6960171af0d726d6b155aa0d53cbf4
zhangmengjie <zmj@qq.com>
2018-11-21 14:08:55 +0800

    B端bug修改

--- src/components/Buser/user/userInfo.vue
+++ src/components/Buser/user/userInfo.vue
@@ -309,7 +309,7 @@
 
       //    树节点点击
       handleNodeClick(item, node, aaa) {
-        console.log(item);
+        item.orgId = item.id;
         if (item.orgId == "0") {
           this.$message({
             message: "顶级组织不可编辑",
